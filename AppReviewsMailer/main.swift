6f00b2905406b40cd46e53eb046b2ba1a0790fdb
Stanislav Sidelnikov <stansidel@yandex-team.ru>
2016-11-11 11:22:49 +0500

    Initial Commit

---
+++ AppReviewsMailer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AppReviewsMailer
+//
+//  Created by Stanislav Sidelnikov on 11/11/16.
+//  Copyright © 2016 StanSidel. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
